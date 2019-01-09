ee5818ea43c56190e73c22f008264b2ce868c10e
HristinaSotirova <chris921@abv.bg>
2019-01-09 18:25:56 +0000

    UI
    added health bar

--- Squiggle Asteroid/Assets/Scripts/HealthUI.cs
+++ Squiggle Asteroid/Assets/Scripts/HealthUI.cs
@@ -5,7 +5,17 @@ using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour {
 
-    public Sprite[] health;
-    public Image hearts;
+    public Sprite[] hearts;
+    public Image Health;
     private PlayerHealth player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    void Update()
+    {
+        Health.sprite = hearts[player.health];
+    }
 }
